10e68bceaae96bd8ce694fa8a86abeadacdde399
mctosin <39802158+mctosin@users.noreply.github.com>
2021-02-26 17:12:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
 	static char* a_string_ptr;
 
 	// Abre um arquivo texto para leitura, 
-	// cujo nome está em argv[1].
+	// cujo nome estÃ¡ em argv[1].
 	// Copia o texto do arquivo para a string a_string_ptr, 
 	// criada dinamicamente.
 	// Fecha o arquivo.
@@ -46,65 +46,42 @@ int main(int argc, char* argv[]) {
 
 	fclose(cfPtr);
 
-	// Não modifique o código acima
+	// NÃ£o modifique o cÃ³digo acima
 
-	// --- Instruções do Programa ---
+	// --- InstruÃ§Ãµes do Programa ---
 	// O ponteiro a_string_prt aponta para a string que foi impressa na tela.
-	// Neste exercício você deve fazer um programa para determinar:
-	// a) A quantidade de caracteres há na string, excluindo o fim de string ('\0').
+	// Neste exercÃ­cio vocÃª deve fazer um programa para determinar:
+	// a) A quantidade de caracteres hÃ¡ na string, excluindo o fim de string ('\0').
 	// b) O tamanho da string em bytes
-	// c) A quantidade de dígitos representados na string (0..9)
+	// c) A quantidade de dÃ­gitos representados na string (0..9)
 	// d) A quantidade de letras representados na string
-	// e) A quantidade de letras maiúsculas na string
-	// f) A quantidade de letras minúsculas na string
-	// g) A quantidade de espaços em branco na string
+	// e) A quantidade de letras maiÃºsculas na string
+	// f) A quantidade de letras minÃºsculas na string
+	// g) A quantidade de espaÃ§os em branco na string
 	// h) A quantidade de caracteres de controle
-	// i) A quantidade de caracteres que representam pontuação
-	// j) A quantidade de caracteres que são imprimíveis na tela (aqueles visíveis na tela),
-	//	  incluindo espaço em branco
-	// Você pode usar, entre outras, as funções ilustradas no livro texto 
+	// i) A quantidade de caracteres que representam pontuaÃ§Ã£o
+	// j) A quantidade de caracteres que sÃ£o imprimÃ­veis na tela (aqueles visÃ­veis na tela),
+	//	  incluindo espaÃ§o em branco
+	// VocÃª pode usar, entre outras, as funÃ§Ãµes ilustradas no livro texto 
 	// (Deitel, C How to Program, 8th ed., Cap.8 - Fig.8.1), definidas em ctype.h
-	// A documentação das funções pode ser encontrada em:
+	// A documentaÃ§Ã£o das funÃ§Ãµes pode ser encontrada em:
 	// https://docs.microsoft.com/pt-br/cpp/c-runtime-library/character-classification?view=msvc-160
 
-	// Faça seu código aqui
+	// FaÃ§a seu cÃ³digo aqui
 
-	int k, val_carct;
-	unsigned int output[10] = { 0 };
 	
-	output[0] = strlen(a_string_ptr);
-	output[1] = output[0] + 1;
+	
+	// --- SaÃ­da dos resultados ---
+	// Os valores dos itens de (a) atÃ© (j) devem ser impressos em decimal, cada qual
+	// separado por UM espaÃ§o em branco.
+	// ApÃ³s o Ãºltimo valor (item j) deve ser impresso um pula linha ('\n'),
+	// ou seja, nÃ£o tem espaÃ§o.
 
-	for (k = 0; a_string_ptr[k] != '\0'; k++) {
-		if (a_string_ptr[k] != EOF) {
-			val_carct = (unsigned char)a_string_ptr[k];
-		}
-		else {
-			val_carct = a_string_ptr[k];
-		}
-		if (isdigit(val_carct)) output[2]++;
-		if (isalpha(val_carct)) output[3]++;
-		if (isupper(val_carct)) output[4]++;
-		if (islower(val_carct)) output[5]++;
-		if (val_carct  ==  ' ') output[6]++;
-		if (iscntrl(val_carct)) output[7]++;
-		if (ispunct(val_carct)) output[8]++;
-		if (isprint(val_carct)) output[9]++;
-	}
+	// FaÃ§a seu cÃ³digo para a saÃ­da dos resultados aqui
 
-	// --- Saída dos resultados ---
-	// Os valores dos itens de (a) até (j) devem ser impressos em decimal, cada qual
-	// separado por UM espaço em branco.
-	// Após o último valor (item j) deve ser impresso um pula linha ('\n'),
-	// ou seja, não tem espaço.
-
-	// Faça seu código para a saída dos resultados aqui
-
-	for (k = 0; k < 10; k++) {
-		printf((k < 9 ? "%d " : "%d\n"), output[k]);
-	}
-
-	// Não modifique o código abaixo
+	
+	
+	// NÃ£o modifique o cÃ³digo abaixo
 
 	return 0;
 }
